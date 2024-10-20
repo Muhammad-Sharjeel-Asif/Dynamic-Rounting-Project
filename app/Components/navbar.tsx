49f36248a921780d59a4a8a3a6e60a312e5832e4
Muhammad Sharjeel Asif <m.sharjeelasif1435@gmail.com>
2024-10-20 15:23:52 +0500

    css changes

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Navbar() {
     return (
         <>
-            <nav className='flex bg-[#f9f6f6e0] justify-between'>
+            <nav className='flex bg-[#f9f6f6e0] justify-between p:100'>
                 <Image src="/favicon.ico" alt="Logo" className='rounded-[2rem] w-14 mx-2 my-2 border-2' width={20} height={20}/>
                 <ul className='flex text-2xl self-center pr-16 gap-12 text-black font-extrabold'>
                     <li className=''><Link href={"/"}>Home</Link></li>
